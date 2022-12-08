78e44980e5f197f30dfc1e3685db0dc10549388c
Revk22 <l19331246@hermosillo.tecnm.mx>
2022-12-08 16:24:16 -0700

    navegacion entre pantallas

--- App.js
+++ App.js
@@ -1,22 +1,62 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Kzimena Tonta</Text>
-      <Text>No cierto el gracias es el tonto</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={styles.titulo}>ELDERLY CARE</Text>
+      <Image source={require('./logo.png')} />
+      <Button
+        title='Iniciar Sesion'
+        style={styles.botton}
+        onPress={() => navigation.navigate('Detalles')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function RegisterScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Pantalla de detalles</Text>
+      <Button
+        title='Continuar →'
+        onPress={() => navigation.push('Loading')} />
+    </View>
+  );
+}
+
+function LoadingScreen({ navigation }) {
+  return (
+    <View>
+      <Text> Loading </Text>
+      <Text> Ya casi terminamos ! </Text>
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'lightblue'
+          },
+          headerTintColor: 'black'
+        }}>
+        <Stack.Screen name="Inicio de Sesión" component={HomeScreen} />
+        <Stack.Screen name="Detalles" component={RegisterScreen} />
+        <Stack.Screen name='Loading' component={LoadingScreen} />
+      </Stack.Navigator>
+      <StatusBar style='auto' />
+    </NavigationContainer>
+  );
+}
---
+++ styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    titulo: {
+        textAlign: 'center',
+        fontSize: 20,
+        //fontFamily: 'Lexend Peta'
+    },
+    boton: {
+        borderColor: 'yellow',
+        borderRadius: 6,
+    }
+});
