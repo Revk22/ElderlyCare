6c6598949d316d38b349d19c5f8da1373f3d215b
Revk22 <l19331246@hermosillo.tecnm.mx>
2022-12-08 18:18:28 -0700

    Actualizado 6pm

--- App.js
+++ App.js
@@ -4,37 +4,67 @@ import { View, Text, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import styles from './styles';
+import { texto, botones, pantalla } from './styles';
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.titulo}>ELDERLY CARE</Text>
-      <Image source={require('./logo.png')} />
-      <Button
-        title='Iniciar Sesion'
-        style={styles.botton}
-        onPress={() => navigation.navigate('Detalles')} />
+    <View style={pantalla.base}>
+      <Text style={texto.titulo}>ELDERLY CARE</Text>
+      <Image style={texto.imagen} source={require('./logo.png')} />
+      <View style={botones.inicio}>
+        <Button
+          title='Iniciar Sesion'
+          onPress={() => navigation.navigate('Inicio de Sesión')} />
+      </View>
+      <View style={botones.inicio}>
+        <Button
+          title='Registrarse'
+          onPress={() => navigation.navigate('Registrate')} />
+      </View>
+    </View>
+  );
+}
+
+function IngresarScreen({ navigation }) {
+  return (
+    <View style={pantalla.base}>
+      <Text style={[texto.titulo, { marginBottom: 75 }]}>INICIO DE SESIÓN</Text>
+      <Text>Número celular o No. de usuario</Text>
+      <View style={botones.continuar}>
+        <Button
+          title='Continuar   →'
+          //** MODIFICAR */ Debe llevar al menú
+          onPress={() => navigation.push('**')} />
+      </View>
     </View>
   );
 }
 
 function RegisterScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Pantalla de detalles</Text>
-      <Button
-        title='Continuar →'
-        onPress={() => navigation.push('Loading')} />
+    <View style={pantalla.base}>
+      <Text> Loading </Text>
+      <Text> Ya casi terminamos ! </Text>
+      <View style={botones.continuar}>
+        <Button
+          title='Continuar   →'
+          onPress={() => navigation.push('Dificultades')} />
+      </View>
     </View>
   );
 }
 
-function LoadingScreen({ navigation }) {
+function DificultadesScreen({ navigation }) {
   return (
-    <View>
+    <View style={pantalla.base}>
       <Text> Loading </Text>
       <Text> Ya casi terminamos ! </Text>
+      <View style={botones.continuar}>
+        <Button
+          title='Continuar   →'
+          //** MODIFICAR */ Debe llevar a lo siguiente del formulario
+          onPress={() => navigation.push('**')} />
+      </View>
     </View>
   );
 }
@@ -45,18 +75,21 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'
+      <Stack.Navigator initialRouteName='Elderly Care'
         screenOptions={{
           headerStyle: {
             backgroundColor: 'lightblue'
           },
           headerTintColor: 'black'
         }}>
-        <Stack.Screen name="Inicio de Sesión" component={HomeScreen} />
-        <Stack.Screen name="Detalles" component={RegisterScreen} />
-        <Stack.Screen name='Loading' component={LoadingScreen} />
+        <Stack.Screen name="Elderly Care" component={HomeScreen} />
+        <Stack.Screen name="Inicio de Sesión" component={IngresarScreen} />
+        <Stack.Screen name='Registrate' component={RegisterScreen} />
+        <Stack.Screen name='Dificultades' component={DificultadesScreen} />
       </Stack.Navigator>
       <StatusBar style='auto' />
     </NavigationContainer>
   );
 }
+
+
--- styles.js
+++ styles.js
@@ -1,13 +1,42 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({
-    titulo: {
-        textAlign: 'center',
-        fontSize: 20,
-        //fontFamily: 'Lexend Peta'
-    },
-    boton: {
-        borderColor: 'yellow',
-        borderRadius: 6,
+const pantalla = StyleSheet.create({
+    base: {
+        flex: 1, 
+        alignItems: 'center', 
+        backgroundColor: 'white' 
     }
 });
+
+const texto = StyleSheet.create({
+    titulo: {
+        textAlign: 'center',
+        fontSize: 30,
+        marginTop: 50,    
+        fontWeight: 'bold',
+        fontFamily: 'sans-serif'   
+        //fontFamily: 'Lexend Peta' //No deja
+    },
+    imagen: {
+        borderRadius: 150,
+        marginTop: 25,
+        width: 170,
+        height: 170,
+        marginBottom: 70,
+    }
+});
+
+const botones = StyleSheet.create({
+    inicio: {
+        marginTop: 45,
+        width: 180,
+        //marginBottom: 45,
+    },
+    continuar: { //Debe ser a un view por lo que veo
+        position: 'absolute',
+        right: 35,
+        bottom: 50,
+    }
+});
+
+export {texto, botones, pantalla};
